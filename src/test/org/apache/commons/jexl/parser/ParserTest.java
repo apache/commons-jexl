4dc2ed4bde6cc3e20085387cb51f057bccfdaa3a
Geir Magnusson Jr <geirm@apache.org>
2002-04-26 04:46:27 +0000

    For basic parser tests
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jexl/trunk@136458 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/test/org/apache/commons/jexl/parser/ParserTest.java
@@ -0,0 +1,50 @@
+package org.apache.commons.jexl.parser;
+
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import junit.framework.Test;
+
+import java.io.StringReader;
+
+import org.apache.commons.jexl.JexlContext;
+import org.apache.commons.jexl.JexlHelper;
+
+/**
+ *
+ */
+public class ParserTest extends TestCase
+{
+    public static Test suite()
+    {
+        return new TestSuite(ParserTest.class);
+    }
+
+    public ParserTest(String testName)
+    {
+        super(testName);
+    }
+
+    /**
+      *  parse test : see if we can parse a little script
+      */
+     public void testParse1()
+         throws Exception
+     {
+         Parser parser = new Parser(new StringReader(";"));
+
+         SimpleNode sn = parser.parse(new StringReader("foo = 1;"));
+
+         JexlContext jc = JexlHelper.createContext();
+
+         sn.interpret(jc);
+     }
+
+    public static void main(String[] args)
+        throws Exception
+    {
+        ParserTest pt = new ParserTest("foo");
+
+        pt.testParse1();
+    }
+
+}
