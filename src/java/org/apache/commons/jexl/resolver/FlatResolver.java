64bd4ec96198de24587b0809d1d7eb6e11573c41
Geir Magnusson Jr <geirm@apache.org>
2002-06-13 16:10:44 +0000

    resolver that just looks up the data from the context w/o regard to structure
    git-svn-id: https://svn-us.apache.org/repos/asf/jakarta/commons/proper/jexl/trunk@136497 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/commons/jexl/resolver/FlatResolver.java
@@ -0,0 +1,52 @@
+package org.apache.commons.jexl.resolver;
+
+import org.apache.commons.jexl.JexlExprResolver;
+import org.apache.commons.jexl.JexlContext;
+
+/**
+ *  Simple resolver to try the expression as-is from the context.
+ *
+ *  For example, you could resolve ant-ish properties (foo.bar.woogie)
+ *  using this...
+ *
+ *  hint, hint...
+ *
+ *  @author <a href="mailto:geirm@adeptra.com">Geir Magnusson Jr.</a>
+ *  @version $Id: FlatResolver.java,v 1.1 2002/06/13 16:10:44 geirm Exp $
+ */
+public class FlatResolver implements JexlExprResolver
+{
+    /**
+     *  flag to return NO_VALUE on null from context
+     *  this allows jexl to try to evaluate
+     */
+    protected boolean noValOnNull = true;
+
+    /**
+     * default CTOR
+     */
+    public FlatResolver()
+    {
+    }
+
+    /**
+     *  CTOR that lets you override the default behavior of
+     *  noValOnNull, which is true (jexl gets a shot after if null)
+     */
+    public FlatResolver(boolean noValOnNull)
+    {
+        this.noValOnNull = noValOnNull;
+    }
+
+    public Object evaluate(JexlContext context, String expression)
+    {
+        Object val = context.getVars().get(expression);
+
+        if (val == null && noValOnNull)
+        {
+            return JexlExprResolver.NO_VALUE;
+        }
+
+        return val;
+    }
+}
