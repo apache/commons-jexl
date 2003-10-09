cdbd99911625015e638efa961edf8d424c1e2844
Robert Burrell Donkin <rdonkin@apache.org>
2003-10-09 21:28:57 +0000

    Fixed typos in apache licenses
    git-svn-id: https://svn-us.apache.org/repos/asf/jakarta/commons/proper/jexl/trunk@136551 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jexl/parser/ASTGTNode.java
+++ src/java/org/apache/commons/jexl/parser/ASTGTNode.java
@@ -17,11 +17,11 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution, if
- *    any, must include the following acknowlegement:
+ *    any, must include the following acknowledgement:
  *       "This product includes software developed by the
  *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowlegement may appear in the software itself,
- *    if and wherever such third-party acknowlegements normally appear.
+ *    Alternately, this acknowledgement may appear in the software itself,
+ *    if and wherever such third-party acknowledgements normally appear.
  *
  * 4. The names "The Jakarta Project", "Commons", "Jexl" and "Apache Software
  *    Foundation" must not be used to endorse or promote products derived
@@ -30,7 +30,7 @@
  *
  * 5. Products derived from this software may not be called "Apache"
  *    nor may "Apache" appear in their names without prior written
- *    permission of the Apache Group.
+ *    permission of the Apache Software Foundation.
  *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -63,7 +63,7 @@ import org.apache.commons.jexl.util.Coercion;
  *
  *  @author <a href="mailto:geirm@apache.org">Geir Magnusson Jr.</a>
  *  @author <a href="mailto:proyal@apache.org">Peter Royal</a>
- *  @version $Id: ASTGTNode.java,v 1.3 2003/06/22 19:43:01 proyal Exp $
+ *  @version $Id: ASTGTNode.java,v 1.4 2003/10/09 21:28:55 rdonkin Exp $
  */
 public class ASTGTNode extends SimpleNode
 {
--- src/java/org/apache/commons/jexl/util/GetExecutor.java
+++ src/java/org/apache/commons/jexl/util/GetExecutor.java
@@ -17,11 +17,11 @@
  *    distribution.
  *
  * 3. The end-user documentation included with the redistribution, if
- *    any, must include the following acknowlegement:
+ *    any, must include the following acknowledgement:
  *       "This product includes software developed by the
  *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowlegement may appear in the software itself,
- *    if and wherever such third-party acknowlegements normally appear.
+ *    Alternately, this acknowledgement may appear in the software itself,
+ *    if and wherever such third-party acknowledgements normally appear.
  *
  * 4. The names "The Jakarta Project", "Velocity", and "Apache Software
  *    Foundation" must not be used to endorse or promote products derived
@@ -30,7 +30,7 @@
  *
  * 5. Products derived from this software may not be called "Apache"
  *    nor may "Apache" appear in their names without prior written
- *    permission of the Apache Group.
+ *    permission of the Apache Software Foundation.
  *
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
@@ -67,7 +67,7 @@ import org.apache.commons.logging.Log;
  * the case.
  *
  * @author <a href="mailto:jvanzyl@apache.org">Jason van Zyl</a>
- * @version $Id: GetExecutor.java,v 1.2 2002/12/16 10:41:59 jstrachan Exp $
+ * @version $Id: GetExecutor.java,v 1.3 2003/10/09 21:28:56 rdonkin Exp $
  */
 public class GetExecutor extends AbstractExecutor
 {
