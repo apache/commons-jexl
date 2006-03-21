3075333d5d9a92a41f4a05291b7ea96eb7ba848d
Dion Gillard <dion@apache.org>
2006-03-21 07:10:33 +0000

    Basic Implementation of bitwise and
    git-svn-id: https://svn-us.apache.org/repos/asf/jakarta/commons/proper/jexl/trunk@387445 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jexl/parser/ASTBitwiseAndNode.java
+++ src/java/org/apache/commons/jexl/parser/ASTBitwiseAndNode.java
@@ -2,6 +2,16 @@
 
 package org.apache.commons.jexl.parser;
 
+import org.apache.commons.jexl.JexlContext;
+import org.apache.commons.jexl.util.Coercion;
+
+/**
+ * Bitwise And. Syntax:
+ *      a & b
+ * Result is a Long
+ * @author Dion Gillard
+ * @since 1.0.1
+ */
 public class ASTBitwiseAndNode extends SimpleNode {
   public ASTBitwiseAndNode(int id) {
     super(id);
@@ -15,5 +25,18 @@ public class ASTBitwiseAndNode extends SimpleNode {
   /** Accept the visitor. **/
   public Object jjtAccept(ParserVisitor visitor, Object data) {
     return visitor.visit(this, data);
+  }
+  
+  /**
+   * @return a {@link Long} which is the bitwise and of the two operands.
+   */
+  public Object value(JexlContext context) throws Exception
+  {
+      Object left = ((SimpleNode) jjtGetChild(0)).value(context);
+      Object right = ((SimpleNode) jjtGetChild(1)).value(context);
+    
+      Long l = left == null ? new Long(0) : Coercion.coerceLong(left);
+      Long r = right == null ? new Long(0) : Coercion.coerceLong(right);
+      return new Long(l.longValue() & r.longValue());
   }
 }
