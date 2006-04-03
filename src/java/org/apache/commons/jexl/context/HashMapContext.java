1f1ad8d5238989a4ffe9b3a225ba867a38b6079c
Dion Gillard <dion@apache.org>
2006-04-03 02:14:05 +0000

    Add a serialVersionUID for serializable classes
    git-svn-id: https://svn-us.apache.org/repos/asf/jakarta/commons/proper/jexl/trunk@390943 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jexl/context/HashMapContext.java
+++ src/java/org/apache/commons/jexl/context/HashMapContext.java
@@ -30,6 +30,8 @@ import java.util.Map;
  */
 public class HashMapContext extends HashMap implements JexlContext
 {
+    /** serialization version id jdk13 generated */
+    static final long serialVersionUID = 5715964743204418854L;
     public void setVars(Map vars)
     {
         clear();
