7f220a7aca3c471a095418695e6a49a548691a4d
Dion Gillard <dion@apache.org>
2006-04-26 01:50:41 +0000

    Checkstyle
    git-svn-id: https://svn-us.apache.org/repos/asf/jakarta/commons/proper/jexl/trunk@397047 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jexl/JexlExprResolver.java
+++ src/java/org/apache/commons/jexl/JexlExprResolver.java
@@ -1,6 +1,6 @@
 /*
  * Copyright 2002,2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -17,24 +17,24 @@ package org.apache.commons.jexl;
 
 /**
  *  A Resolver allows custom resolution of the expression, and can be
- *  added in front of the jexl engine, or after in the evaluation
+ *  added in front of the jexl engine, or after in the evaluation.
  *
  *  @todo This needs to be explained in detail.  Why do this?
  *  @since 1.0
  *  @author <a href="mailto:geirm@adeptra.com">Geir Magnusson Jr.</a>
  *  @version $Id$
  */
-public interface JexlExprResolver
-{
+public interface JexlExprResolver {
+    /** represents an expression result of no value. */ 
     Object NO_VALUE = new Object();
 
     /**
-     *  evaluates an expression against the context
+     *  Evaluates an expression against the context.
      *
      *  @todo Must detail the expectations and effects of this resolver.
      *  @param context current data context
      *  @param expression expression to evauluate
-     *  @return value (may be null) po the NO_VALUE object to
+     *  @return value (may be null) or the NO_VALUE object to
      *       indicate no resolution.
      */
     Object evaluate(JexlContext context, String expression);
