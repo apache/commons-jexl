d18e3b1ae668e4f334bfd3106f6a09b58fbf7698
Dion Gillard <dion@apache.org>
2006-04-26 06:27:48 +0000

    Checkstyle
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jexl/trunk@397104 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jexl/ScriptFactory.java
+++ src/java/org/apache/commons/jexl/ScriptFactory.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002-2006 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -55,10 +55,11 @@ public class ScriptFactory {
         LogFactory.getLog("org.apache.commons.jexl.ScriptFactory");
 
     /**
-     * The singleton ScriptFactory also holds a single instance of {@link Parser}.
-     * When parsing expressions, ScriptFactory synchronizes on Parser.
+     * The singleton ScriptFactory also holds a single instance of 
+     * {@link Parser}. When parsing expressions, ScriptFactory 
+     * synchronizes on Parser.
      */
-    protected static Parser parser = new Parser(new StringReader(";")); //$NON-NLS-1$
+    protected static Parser parser = new Parser(new StringReader(";"));
 
     /**
      * ScriptFactory is a singleton and this is the private
@@ -70,14 +71,15 @@ public class ScriptFactory {
      * Private constructor, the single instance is always obtained
      * with a call to getInstance().
      */
-    private ScriptFactory(){}
+    private ScriptFactory() {
+
+    }
 
     /**
      * Returns the single instance of ScriptFactory.
      * @return the instance of ScriptFactory.
      */
-    protected static  ScriptFactory getInstance()
-    {
+    protected static  ScriptFactory getInstance() {
         return factory;
     }
 
@@ -86,11 +88,12 @@ public class ScriptFactory {
      * This method parses the script which validates the syntax.
      * 
      * @param scriptText A String containing valid JEXL syntax
-     * @return A {@link Script} which can be executed with a {@link JexlContext}.
-     * @throws Exception An exception can be thrown if there is a problem parsing the script.
+     * @return A {@link Script} which can be executed with a 
+     *      {@link JexlContext}.
+     * @throws Exception An exception can be thrown if there is a 
+     *      problem parsing the script.
      */
-    public static Script createScript(String scriptText) throws Exception
-    {
+    public static Script createScript(String scriptText) throws Exception {
         return getInstance().newScript(scriptText);
     }
 
@@ -98,17 +101,20 @@ public class ScriptFactory {
      * Creates a Script from a {@link File} containing valid JEXL syntax. 
      * This method parses the script and validates the syntax.
      * 
-     * @param scriptFile A {@link File} containing valid JEXL syntax. Must not be null. Must be a readable file.
-     * @return A {@link Script} which can be executed with a {@link JexlContext}.
-     * @throws Exception An exception can be thrown if there is a problem parsing the script.
+     * @param scriptFile A {@link File} containing valid JEXL syntax.
+     *      Must not be null. Must be a readable file.
+     * @return A {@link Script} which can be executed with a 
+     *      {@link JexlContext}.
+     * @throws Exception An exception can be thrown if there is a problem 
+     *      parsing the script.
      */
-    public static Script createScript(File scriptFile) throws Exception
-    {
+    public static Script createScript(File scriptFile) throws Exception {
         if (scriptFile == null) {
-            throw new NullPointerException("scriptFile passed to ScriptFactory.createScript is null");
+            throw new NullPointerException("scriptFile is null");
         }
         if (!scriptFile.canRead()) {
-            throw new IOException("Can't read scriptFile (" + scriptFile.getCanonicalPath() +")");
+            throw new IOException("Can't read scriptFile (" 
+                + scriptFile.getCanonicalPath() + ")");
         }
         BufferedReader reader = new BufferedReader(new FileReader(scriptFile));
         return createScript(readerToString(reader));
@@ -119,18 +125,21 @@ public class ScriptFactory {
      * Creates a Script from a {@link URL} containing valid JEXL syntax. 
      * This method parses the script and validates the syntax.
      * 
-     * @param scriptUrl A {@link URL} containing valid JEXL syntax. Must not be null. Must be a readable file.
-     * @return A {@link Script} which can be executed with a {@link JexlContext}.
-     * @throws Exception An exception can be thrown if there is a problem parsing the script.
+     * @param scriptUrl A {@link URL} containing valid JEXL syntax.
+     *      Must not be null. Must be a readable file.
+     * @return A {@link Script} which can be executed with a 
+     *      {@link JexlContext}.
+     * @throws Exception An exception can be thrown if there is a problem
+     *      parsing the script.
      */
-    public static Script createScript(URL scriptUrl) throws Exception
-    {
+    public static Script createScript(URL scriptUrl) throws Exception {
         if (scriptUrl == null) {
-            throw new NullPointerException("scriptUrl passed to ScriptFactory.createScript is null");
+            throw new NullPointerException("scriptUrl is null");
         }
         URLConnection connection = scriptUrl.openConnection();
         
-        BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        BufferedReader reader = new BufferedReader(
+            new InputStreamReader(connection.getInputStream()));
         return createScript(readerToString(reader));
     }
 
@@ -144,14 +153,14 @@ public class ScriptFactory {
     protected Script newScript(String scriptText) throws Exception {
         String cleanText = cleanScript(scriptText);
         // Parse the Expression
-        synchronized(parser)
-        {
+        synchronized (parser) {
             log.debug("Parsing script: " + cleanText);
             SimpleNode script = parser.parse(new StringReader(cleanText));
             if (script instanceof ASTJexlScript) {
                 return new ScriptImpl(cleanText, (ASTJexlScript) script);
             } else {
-                throw new IllegalStateException("Parsed script is not an ASTJexlScript");
+                throw new IllegalStateException("Parsed script is not "
+                    + "an ASTJexlScript");
             }
         }
     }
@@ -159,24 +168,26 @@ public class ScriptFactory {
     /**
      * @todo move to ParseUtils?
      * Trims the expression and adds a semi-colon if missing.
-     * @param expression to clean
+     * @param script to clean
      * @return trimmed expression ending in a semi-colon
      */
     private String cleanScript(String script) {
         String expr = script.trim();
-        if (!expr.endsWith(";"))
-        {
+        if (!expr.endsWith(";")) {
             expr += ";";
         }
         return expr;
     }
     
     /**
-     * Read a buffered reader into a StringBuffer and return a String with the contents of the reader.
+     * Read a buffered reader into a StringBuffer and return a String with
+     * the contents of the reader.
+     * @param reader to be read.
      * @return the contents of the reader as a String.
      * @throws IOException on any error reading the reader.
      */
-    private static String readerToString(BufferedReader reader) throws IOException {
+    private static String readerToString(BufferedReader reader)
+        throws IOException {
         StringBuffer buffer = new StringBuffer();
         try {
             String line = null;
--- src/java/org/apache/commons/jexl/ScriptImpl.java
+++ src/java/org/apache/commons/jexl/ScriptImpl.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002,2004 The Apache Software Foundation.
- * 
+ * Copyright 2002-2006 The Apache Software Foundation.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -24,11 +24,16 @@ import org.apache.commons.jexl.parser.ASTJexlScript;
  */
 class ScriptImpl implements Script {
 
-    /** text of the script */
+    /** text of the script. */
     private final String text;
-    /** syntax tree */
+    /** syntax tree. */
     private final ASTJexlScript parsedScript;
     
+    /**
+     * Create a new Script from the given string and parsed syntax.
+     * @param scriptText the text of the script.
+     * @param scriptTree the parsed script.
+     */
     public ScriptImpl(String scriptText, ASTJexlScript scriptTree) {
         text = scriptText;
         parsedScript = scriptTree;
@@ -36,6 +41,9 @@ class ScriptImpl implements Script {
 
     /**
      * @see Script#execute(JexlContext)
+     * @return the value of the script. Usually the value of the last 
+     *      executed statement. 
+     * @throws Exception on any error.
      */
     public Object execute(JexlContext context) throws Exception {
         // TODO Auto-generated method stub
