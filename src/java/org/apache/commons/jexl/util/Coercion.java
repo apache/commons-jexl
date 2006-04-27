fb85d4159f893c6477eaa491575de86580878653
Rahul Akolkar <rahul@apache.org>
2006-04-27 07:09:13 +0000

    Conform to checkstyle, add missing Javadocs. No functional change.
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jexl/trunk@397454 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jexl/util/Coercion.java
+++ src/java/org/apache/commons/jexl/util/Coercion.java
@@ -21,97 +21,94 @@ package org.apache.commons.jexl.util;
  *  @since 1.0
  *  @author <a href="mailto:geirm@apache.org">Geir Magnusson Jr.</a>
  */
-public class Coercion
-{
+public class Coercion {
 
+    /**
+     * Coerce to a Boolean.
+     *
+     * @param val Object to be coerced.
+     * @return The Boolean coerced value, or null if none possible.
+     */
     public static Boolean coerceBoolean(Object val) {
-        if (val == null)
-        {
+        if (val == null) {
             return Boolean.FALSE;
-        }
-        else if (val instanceof Boolean)
-        {
+        } else if (val instanceof Boolean) {
             return (Boolean) val;
-        }
-        else if (val instanceof String)
-        {
+        } else if (val instanceof String) {
             return Boolean.valueOf((String) val);
         }
-
         return null;
     }
 
+    /**
+     * Coerce to a Integer.
+     *
+     * @param val Object to be coerced.
+     * @return The Integer coerced value.
+     * @throws Exception If Integer coercion fails.
+     */
     public static Integer coerceInteger(Object val)
-        throws Exception
-    {
-        if (val == null)
-        {
+    throws Exception {
+        if (val == null) {
             return new Integer(0);
-        }
-        else if (val instanceof String)
-        {
-            if("".equals(val))
+        } else if (val instanceof String) {
+            if ("".equals(val)) {
                 return new Integer(0);
-
-            return Integer.valueOf((String)val);
-        }
-        else if(val instanceof Character)
-        {
-            return new Integer(((Character)val).charValue());
-        }
-        else if(val instanceof Boolean)
-        {
+            }
+            return Integer.valueOf((String) val);
+        } else if (val instanceof Character) {
+            return new Integer(((Character) val).charValue());
+        } else if (val instanceof Boolean) {
             throw new Exception("Boolean->Integer coercion exception");
-        }
-        else if(val instanceof Number)
-        {
-            return new Integer(((Number)val).intValue());
+        } else if (val instanceof Number) {
+            return new Integer(((Number) val).intValue());
         }
 
         throw new Exception("Integer coercion exception");
     }
 
+    /**
+     * Coerce to a Long.
+     *
+     * @param val Object to be coerced.
+     * @return The Long coerced value.
+     * @throws Exception If Long coercion fails.
+     */
     public static Long coerceLong(Object val)
-        throws Exception
-    {
-        if (val == null)
-        {
+    throws Exception {
+        if (val == null) {
             return new Long(0);
-        }
-        else if (val instanceof String)
-        {
-            if("".equals(val))
+        } else if (val instanceof String) {
+            if ("".equals(val)) {
                 return new Long(0);
-
-            return Long.valueOf((String)val);
-        }
-        else if(val instanceof Character)
-        {
-            return new Long(((Character)val).charValue());
-        }
-        else if(val instanceof Boolean)
-        {
+            }
+            return Long.valueOf((String) val);
+        } else if (val instanceof Character) {
+            return new Long(((Character) val).charValue());
+        } else if (val instanceof Boolean) {
             throw new Exception("Boolean->Integer coercion exception");
-        }
-        else if(val instanceof Number)
-        {
-            return new Long(((Number)val).longValue());
+        } else if (val instanceof Number) {
+            return new Long(((Number) val).longValue());
         }
 
         throw new Exception("Long coercion exception");
     }
 
+    /**
+     * Coerce to a Double.
+     *
+     * @param val Object to be coerced.
+     * @return The Double coerced value.
+     * @throws Exception If Double coercion fails.
+     */
     public static Double coerceDouble(Object val)
-        throws Exception
-    {
-        if (val == null)
-        {
+    throws Exception {
+        if (val == null) {
             return new Double(0);
-        }
-        else if (val instanceof String)
-        {
-            if("".equals(val))
+        } else if (val instanceof String) {
+            if ("".equals(val)) {
                 return new Double(0);
+            }
 
             /*
              * the spec seems to be iffy about this.  Going to give it a wack
@@ -119,23 +116,15 @@ public class Coercion
              */
 
             return new Double((String) val);
-        }
-        else if(val instanceof Character)
-        {
-            int i = ((Character)val).charValue();
+        } else if (val instanceof Character) {
+            int i = ((Character) val).charValue();
 
             return new Double(Double.parseDouble(String.valueOf(i)));
-        }
-        else if(val instanceof Boolean)
-        {
+        } else if (val instanceof Boolean) {
             throw new Exception("Boolean->Integer coercion exception");
-        }
-        else if(val instanceof Double)
-        {
+        } else if (val instanceof Double) {
             return (Double) val;
-        }
-        else if (val instanceof Number)
-        {
+        } else if (val instanceof Number) {
             //The below construct is used rather than ((Number)val).doubleValue() to ensure
             //equality between comparint new Double( 6.4 / 3 ) and the jexl expression of 6.4 / 3
             return new Double(Double.parseDouble(String.valueOf(val)));
@@ -144,13 +133,23 @@ public class Coercion
         throw new Exception("Double coercion exception");
     }
 
-    public static boolean isFloatingPoint( final Object o )
-    {
+    /**
+     * Is Object a floating point number.
+     *
+     * @param o Object to be analyzed.
+     * @return true if it is a Float or a Double.
+     */
+    public static boolean isFloatingPoint(final Object o) {
         return o instanceof Float || o instanceof Double;
     }
 
-    public static boolean isNumberable( final Object o )
-    {
+    /**
+     * Is Object a whole number.
+     *
+     * @param o Object to be analyzed.
+     * @return true if Integer, Long, Byte, Short or Character.
+     */
+    public static boolean isNumberable(final Object o) {
         return o instanceof Integer
             || o instanceof Long
             || o instanceof Byte
