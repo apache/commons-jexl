da38ae0891ab6a53ae01da33c9b7eac4b4650163
Dion Gillard <dion@apache.org>
2006-04-27 04:11:11 +0000

    More checkstyle
    git-svn-id: https://svn-us.apache.org/repos/asf/jakarta/commons/proper/jexl/trunk@397382 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jexl/parser/ASTAndNode.java
+++ src/java/org/apache/commons/jexl/parser/ASTAndNode.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002,2004 The Apache Software Foundation.
- * 
+ * Copyright 2002-2006 The Apache Software Foundation.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,43 +19,34 @@ import org.apache.commons.jexl.JexlContext;
 import org.apache.commons.jexl.util.Coercion;
 
 /**
- *  && and 'and'
- *
- *  @author <a href="mailto:geirm@apache.org">Geir Magnusson Jr.</a>
- *  @version $Id$
+ * && and 'and'.
+ * 
+ * @author <a href="mailto:geirm@apache.org">Geir Magnusson Jr.</a>
+ * @version $Id$
  */
-public class ASTAndNode extends SimpleNode
-{
-    public ASTAndNode(int id)
-    {
+public class ASTAndNode extends SimpleNode {
+    public ASTAndNode(int id) {
         super(id);
     }
 
-    public ASTAndNode(Parser p, int id)
-    {
+    public ASTAndNode(Parser p, int id) {
         super(p, id);
     }
 
-
-    /** Accept the visitor. **/
-    public Object jjtAccept(ParserVisitor visitor, Object data)
-    {
+    /** Accept the visitor. * */
+    public Object jjtAccept(ParserVisitor visitor, Object data) {
         return visitor.visit(this, data);
     }
 
-    public Object value(JexlContext jc)
-        throws Exception
-    {
+    public Object value(JexlContext jc) throws Exception {
         Object left = ((SimpleNode) jjtGetChild(0)).value(jc);
-        boolean leftValue = Coercion.coerceBoolean(left).booleanValue(); 
+        boolean leftValue = Coercion.coerceBoolean(left).booleanValue();
 
         /*
          * coercion rules
          */
-        return (leftValue
-        		&& 
-				Coercion.coerceBoolean(
-						((SimpleNode) jjtGetChild(1)).value(jc)).booleanValue()) ?
-            Boolean.TRUE : Boolean.FALSE;
+        return (leftValue && Coercion.coerceBoolean(
+                ((SimpleNode) jjtGetChild(1)).value(jc)).booleanValue()) ? Boolean.TRUE
+                : Boolean.FALSE;
     }
 }
