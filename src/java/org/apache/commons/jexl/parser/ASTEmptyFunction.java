35edfa92fd49072689b00d4422c9eb422f78d136
Dion Gillard <dion@apache.org>
2006-04-27 05:43:21 +0000

    More checkstyle
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jexl/trunk@397416 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jexl/parser/ASTEmptyFunction.java
+++ src/java/org/apache/commons/jexl/parser/ASTEmptyFunction.java
@@ -1,12 +1,12 @@
 /*
- * Copyright 2002,2004 The Apache Software Foundation.
- * 
+ * Copyright 2002-2006 The Apache Software Foundation.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,33 +22,30 @@ import java.util.Collection;
 import java.util.Map;
 
 /**
- *  function to see if reference doesn't exist in context
- *
- *  @author <a href="mailto:geirm@apache.org">Geir Magnusson Jr.</a>
- *  @author <a href="mailto:tobrien@apache.org">Tim O'Brien</a>
- *  @version $Id$
+ * function to see if reference doesn't exist in context.
+ * 
+ * @author <a href="mailto:geirm@apache.org">Geir Magnusson Jr.</a>
+ * @author <a href="mailto:tobrien@apache.org">Tim O'Brien</a>
+ * @version $Id$
  */
-public class ASTEmptyFunction extends SimpleNode
-{
-    public ASTEmptyFunction(int id)
-    {
+public class ASTEmptyFunction extends SimpleNode {
+    /** {@inheritDoc} */
+    public ASTEmptyFunction(int id) {
         super(id);
     }
 
-    public ASTEmptyFunction(Parser p, int id)
-    {
+    /** {@inheritDoc} */
+    public ASTEmptyFunction(Parser p, int id) {
         super(p, id);
     }
 
-    /** Accept the visitor. **/
-    public Object jjtAccept(ParserVisitor visitor, Object data)
-    {
+    /** {@inheritDoc} */
+    public Object jjtAccept(ParserVisitor visitor, Object data) {
         return visitor.visit(this, data);
     }
 
-    public Object value(JexlContext jc)
-        throws Exception
-    {
+    /** {@inheritDoc} */
+    public Object value(JexlContext jc) throws Exception {
         SimpleNode sn = (SimpleNode) jjtGetChild(0);
 
         /*
@@ -57,23 +54,28 @@ public class ASTEmptyFunction extends SimpleNode
 
         Object o = sn.value(jc);
 
-        if (o == null)
+        if (o == null) {
             return Boolean.TRUE;
+        }
 
-        if (o instanceof String && "".equals(o))
+        if (o instanceof String && "".equals(o)) {
             return Boolean.TRUE;
+        }
 
-        if (o.getClass().isArray() && ((Object[])o).length == 0)
+        if (o.getClass().isArray() && ((Object[]) o).length == 0) {
             return Boolean.TRUE;
+        }
 
-        if (o instanceof Collection && ((Collection)o).isEmpty())
+        if (o instanceof Collection && ((Collection) o).isEmpty()) {
             return Boolean.TRUE;
+        }
 
         /*
-         *  Map isn't a collection
+         * Map isn't a collection
          */
-        if (o instanceof Map && ((Map)o).isEmpty())
+        if (o instanceof Map && ((Map) o).isEmpty()) {
             return Boolean.TRUE;
+        }
 
         return Boolean.FALSE;
     }
