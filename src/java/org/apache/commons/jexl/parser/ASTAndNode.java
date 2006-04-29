f355641071a63676e338e9df6dbfd046caca8ce3
Dion Gillard <dion@apache.org>
2006-04-29 15:40:35 +0000

    Checkstyle
    git-svn-id: https://svn-us.apache.org/repos/asf/jakarta/commons/proper/jexl/trunk@398180 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jexl/parser/ASTAndNode.java
+++ src/java/org/apache/commons/jexl/parser/ASTAndNode.java
@@ -25,19 +25,31 @@ import org.apache.commons.jexl.util.Coercion;
  * @version $Id$
  */
 public class ASTAndNode extends SimpleNode {
+    /**
+     * Create the node given an id.
+     * 
+     * @param id node id.
+     */
     public ASTAndNode(int id) {
         super(id);
     }
 
+    /**
+     * Create a node with the given parser and id.
+     * 
+     * @param p a parser.
+     * @param id node id.
+     */
     public ASTAndNode(Parser p, int id) {
         super(p, id);
     }
 
-    /** Accept the visitor. * */
+    /** {@inheritDoc} */
     public Object jjtAccept(ParserVisitor visitor, Object data) {
         return visitor.visit(this, data);
     }
 
+    /** {@inheritDoc} */
     public Object value(JexlContext jc) throws Exception {
         Object left = ((SimpleNode) jjtGetChild(0)).value(jc);
         boolean leftValue = Coercion.coerceBoolean(left).booleanValue();
