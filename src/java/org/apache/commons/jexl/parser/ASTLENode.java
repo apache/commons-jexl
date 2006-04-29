e00b0380ef6439bf6d874fb77d33a07ad2988844
Dion Gillard <dion@apache.org>
2006-04-29 16:44:55 +0000

    Checkstyle
    git-svn-id: https://svn-us.apache.org/repos/asf/jakarta/commons/proper/jexl/trunk@398203 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jexl/parser/ASTLENode.java
+++ src/java/org/apache/commons/jexl/parser/ASTLENode.java
@@ -28,19 +28,31 @@ import org.apache.commons.jexl.util.Coercion;
  * @version $Id$
  */
 public class ASTLENode extends SimpleNode {
+    /**
+     * Create the node given an id.
+     * 
+     * @param id node id.
+     */
     public ASTLENode(int id) {
         super(id);
     }
 
+    /**
+     * Create a node with the given parser and id.
+     * 
+     * @param p a parser.
+     * @param id node id.
+     */
     public ASTLENode(Parser p, int id) {
         super(p, id);
     }
 
-    /** Accept the visitor. * */
+    /** {@inheritDoc} */
     public Object jjtAccept(ParserVisitor visitor, Object data) {
         return visitor.visit(this, data);
     }
 
+    /** {@inheritDoc} */
     public Object value(JexlContext jc) throws Exception {
         /*
          * now get the values
--- src/java/org/apache/commons/jexl/parser/ASTLTNode.java
+++ src/java/org/apache/commons/jexl/parser/ASTLTNode.java
@@ -29,19 +29,31 @@ import org.apache.commons.jexl.util.Coercion;
  * @version $Id$
  */
 public class ASTLTNode extends SimpleNode {
+    /**
+     * Create the node given an id.
+     * 
+     * @param id node id.
+     */
     public ASTLTNode(int id) {
         super(id);
     }
 
+    /**
+     * Create a node with the given parser and id.
+     * 
+     * @param p a parser.
+     * @param id node id.
+     */
     public ASTLTNode(Parser p, int id) {
         super(p, id);
     }
 
-    /** Accept the visitor. * */
+    /** {@inheritDoc} */
     public Object jjtAccept(ParserVisitor visitor, Object data) {
         return visitor.visit(this, data);
     }
 
+    /** {@inheritDoc} */
     public Object value(JexlContext jc) throws Exception {
         /*
          * now get the values
