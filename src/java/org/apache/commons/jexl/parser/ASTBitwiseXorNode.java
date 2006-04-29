82c74b24d21e473ce4490f7bbdaf26172da9f575
Dion Gillard <dion@apache.org>
2006-04-29 15:58:45 +0000

    Checkstyle
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jexl/trunk@398187 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jexl/parser/ASTBitwiseXorNode.java
+++ src/java/org/apache/commons/jexl/parser/ASTBitwiseXorNode.java
@@ -26,21 +26,34 @@ import org.apache.commons.jexl.util.Coercion;
  * @since 1.1
  */
 public class ASTBitwiseXorNode extends SimpleNode {
+    /**
+     * Create the node given an id.
+     * 
+     * @param id node id.
+     */
     public ASTBitwiseXorNode(int id) {
         super(id);
     }
 
+    /**
+     * Create a node with the given parser and id.
+     * 
+     * @param p a parser.
+     * @param id node id.
+     */
     public ASTBitwiseXorNode(Parser p, int id) {
         super(p, id);
     }
 
-    /** Accept the visitor. * */
+    /** 
+     * {@inheritDoc}
+     */
     public Object jjtAccept(ParserVisitor visitor, Object data) {
         return visitor.visit(this, data);
     }
 
-    /**
-     * @return a {@link Long} which is the bitwise xor of the two operands.
+    /** 
+     * {@inheritDoc}
      */
     public Object value(JexlContext context) throws Exception {
         Object left = ((SimpleNode) jjtGetChild(0)).value(context);
--- src/java/org/apache/commons/jexl/parser/ASTDivNode.java
+++ src/java/org/apache/commons/jexl/parser/ASTDivNode.java
@@ -25,19 +25,31 @@ import org.apache.commons.jexl.JexlContext;
  * @version $Id$
  */
 public class ASTDivNode extends SimpleNode {
+    /**
+     * Create the node given an id.
+     * 
+     * @param id node id.
+     */
     public ASTDivNode(int id) {
         super(id);
     }
 
+    /**
+     * Create a node with the given parser and id.
+     * 
+     * @param p a parser.
+     * @param id node id.
+     */
     public ASTDivNode(Parser p, int id) {
         super(p, id);
     }
 
-    /** Accept the visitor. * */
+    /** {@inheritDoc} */
     public Object jjtAccept(ParserVisitor visitor, Object data) {
         return visitor.visit(this, data);
     }
 
+    /** {@inheritDoc} */
     public Object value(JexlContext jc) throws Exception {
         Object left = ((SimpleNode) jjtGetChild(0)).value(jc);
         Object right = ((SimpleNode) jjtGetChild(1)).value(jc);
