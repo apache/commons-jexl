eccfe0e77b236a8ecef212f0b59d6c439b920302
Dion Gillard <dion@apache.org>
2006-04-30 12:57:07 +0000

    Checkstyle
    git-svn-id: https://svn-us.apache.org/repos/asf/jakarta/commons/proper/jexl/trunk@398333 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jexl/util/introspection/VelMethod.java
+++ src/java/org/apache/commons/jexl/util/introspection/VelMethod.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002,2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,42 +16,38 @@
 
 package org.apache.commons.jexl.util.introspection;
 
-
-
 /**
- *  Method used for regular method invocation
- *
- *    $foo.bar()
- *
- *
+ * Method used for regular method invocation
+ * 
+ * $foo.bar()
+ * 
+ * 
  * @since 1.0
  * @author <a href="mailto:geirm@optonline.net">Geir Magnusson Jr.</a>
  * @version $Id$
  */
-public interface VelMethod
-{
+public interface VelMethod {
     /**
-     *  invocation method - called when the method invocationshould be
-     *  preformed and a value returned
+     * invocation method - called when the method invocationshould be preformed
+     * and a value returned
      */
-    public Object invoke(Object o, Object[] params)
-        throws Exception;
+    Object invoke(Object o, Object[] params) throws Exception;
 
     /**
-     *  specifies if this VelMethod is cacheable and able to be
-     *  reused for this class of object it was returned for
-     *
-     *  @return true if can be reused for this class, false if not
+     * specifies if this VelMethod is cacheable and able to be reused for this
+     * class of object it was returned for
+     * 
+     * @return true if can be reused for this class, false if not
      */
-    public boolean isCacheable();
+    boolean isCacheable();
 
     /**
-     *  returns the method name used
+     * returns the method name used
      */
-    public String getMethodName();
+    String getMethodName();
 
     /**
-     *  returns the return type of the method invoked
+     * returns the return type of the method invoked
      */
-    public Class getReturnType();
+    Class getReturnType();
 }
