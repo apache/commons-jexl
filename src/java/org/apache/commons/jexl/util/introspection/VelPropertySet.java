b6590cfc05f4addae1065a60c7190d3ae6e50a9f
Dion Gillard <dion@apache.org>
2006-05-01 03:46:33 +0000

    Checkstyle
    git-svn-id: https://svn.apache.org/repos/asf/jakarta/commons/proper/jexl/trunk@398515 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/commons/jexl/util/introspection/VelPropertySet.java
+++ src/java/org/apache/commons/jexl/util/introspection/VelPropertySet.java
@@ -1,12 +1,12 @@
 /*
  * Copyright 2002,2004 The Apache Software Foundation.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -16,38 +16,38 @@
 
 package org.apache.commons.jexl.util.introspection;
 
-
 /**
- *  Interface used for setting values that appear to be properties in
- *  Velocity.  Ex.
- *
- *      #set($foo.bar = "hello")
- *
+ * Interface used for setting values that appear to be properties in Velocity.
+ * Ex.
+ * 
+ * #set($foo.bar = "hello")
+ * 
  * @since 1.0
  * @author <a href="mailto:geirm@optonline.net">Geir Magnusson Jr.</a>
  * @version $Id$
  */
-public interface VelPropertySet
-{
+public interface VelPropertySet {
     /**
-     *  method used to set the value in the object
-     *
-     *  @param o Object on which the method will be called with the arg
-     *  @param arg value to be set
-     *  @return the value returned from the set operation (impl specific)
+     * method used to set the value in the object.
+     * 
+     * @param o Object on which the method will be called with the arg
+     * @param arg value to be set
+     * @return the value returned from the set operation (impl specific)
+     * @throws Exception on any error.
      */
-    public Object invoke(Object o, Object arg) throws Exception;
+    Object invoke(Object o, Object arg) throws Exception;
 
     /**
-     *  specifies if this VelPropertySet is cacheable and able to be
-     *  reused for this class of object it was returned for
-     *
-     *  @return true if can be reused for this class, false if not
+     * specifies if this VelPropertySet is cacheable and able to be reused for
+     * this class of object it was returned for.
+     * 
+     * @return true if can be reused for this class, false if not
      */
-    public boolean isCacheable();
+    boolean isCacheable();
 
     /**
-     *  returns the method name used to set this 'property'
+     * returns the method name used to set this 'property'.
+     * @return the method name.
      */
-    public String getMethodName();
+    String getMethodName();
 }
