73b5da0b6eef9280110bfc0457251e4512aef8ba
Henri Biestro <henrib@apache.org>
2011-08-03 13:17:44 +0000

    JEXL-118 * Extended =~ and  !~ operator to behave as IN and NOT IN on sets, collections, maps (on keys), arrays and "duck-typed" collections (classes exposing "contains" method) * Updated ArrayListWrapper to allow better control over its usage as method declaring class * Added specific tests * Updated syntax reference * Updated changes
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/jexl/trunk@1153476 13f79535-47bb-0310-9956-ffa450edef68

--- src/main/java/org/apache/commons/jexl2/internal/ArrayListWrapper.java
+++ src/main/java/org/apache/commons/jexl2/internal/ArrayListWrapper.java
@@ -18,9 +18,18 @@ package org.apache.commons.jexl2.internal;
 
 import java.lang.reflect.Array;
 import java.util.AbstractList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 /**
- * A class that wraps an array with a List interface.
+ * A class that wraps an array within an AbstractList.
+ * <p>
+ * It overrides all methods because introspection uses this class a a marker for wrapped arrays; the declared class
+ * for any method is thus always ArrayListWrapper.
+ * </p>
  */
 public class ArrayListWrapper extends AbstractList<Object> {
     /** the array to wrap. */
@@ -56,4 +65,136 @@ public class ArrayListWrapper extends AbstractList<Object> {
     public int size() {
         return Array.getLength(array);
     }
+
+    @Override
+    public Object[] toArray() {
+        final int size = size();
+        Object[] a = new Object[size];
+        for(int i = 0; i < size; ++i) {
+            a[i] = get(i);
+        }
+        return a;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T[] toArray(T[] a) {
+        int size = size();
+        if (a.length < size) {
+            T[] x = (T[]) Array.newInstance(a.getClass().getComponentType(), size);
+            System.arraycopy(a, a.length, x, 0, a.length);
+        }
+        for(int i = 0; i < size; ++i) {
+            a[i] = (T) get(i);
+        }
+        if (a.length > size) {
+            a[size] = null;
+        }
+        return a;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        final int size = size();
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (get(i) == null) {
+                    return i;
+                }
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (o.equals(get(i))) {
+                    return i;
+                }
+            }
+        }
+        return -1;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return indexOf(o) != -1;
+    }
+    
+    @Override
+    public boolean isEmpty() {
+        return super.isEmpty();
+    }
+
+    @Override
+    public Iterator<Object> iterator() {
+        return super.iterator();
+    }
+    
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return super.containsAll(c);
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return super.lastIndexOf(o);
+    }
+
+    @Override
+    public ListIterator<Object> listIterator() {
+        return super.listIterator();
+    }
+
+    @Override
+    public ListIterator<Object> listIterator(int index) {
+        return super.listIterator(index);
+    }
+
+    @Override
+    public List<Object> subList(int fromIndex, int toIndex) {
+        return super.subList(fromIndex, toIndex);
+    }
+    
+    @Override
+    public boolean add(Object o) {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends Object> c) {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends Object> c) {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public void add(int index, Object element) {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
+    @Override
+    public Object remove(int index) {
+        throw new UnsupportedOperationException("Not supported.");
+    }
+
 }
