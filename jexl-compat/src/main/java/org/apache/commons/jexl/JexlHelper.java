e28fc6592b985dceed4722a51d1556d8447e5892
Henri Biestro <henrib@apache.org>
2011-11-29 16:22:56 +0000

    JEXL-122: moved code to o.a.c.jexl3 / move jexl2-compat to jexl-compat
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/jexl/trunk@1207943 13f79535-47bb-0310-9956-ffa450edef68

---
+++ jexl-compat/src/main/java/org/apache/commons/jexl/JexlHelper.java
@@ -0,0 +1,58 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.commons.jexl;
+
+import org.apache.commons.jexl.context.HashMapContext;
+
+/**
+ *  Helper to create a context.  In the current implementation of JEXL, there
+ *  is one implementation of JexlContext - {@link HashMapContext}, and there
+ *  is no reason not to directly instantiate {@link HashMapContext} in your
+ *  own application.
+ *
+ *  @since 1.0
+ *  
+ *  @version $Id$
+ */
+public class JexlHelper {
+    /** singleton instance. */
+    protected static JexlHelper helper = new JexlHelper();
+
+    /** @return the single instance. */
+    protected static JexlHelper getInstance() {
+        return helper;
+    }
+
+    /**
+     * Returns a new {@link JexlContext}.
+     * @return a new JexlContext
+     */
+    public static JexlContext createContext() {
+        return getInstance().newContext();
+    }
+
+    /**
+     * Creates and returns a new {@link JexlContext}.  
+     * The current implementation creates a new instance of 
+     * {@link HashMapContext}.
+     * @return a new JexlContext
+     */
+    protected JexlContext newContext() {
+        return new HashMapContext();
+    }
+}
