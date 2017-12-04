258e5a8e158545fae847ece5a8c3aa5714603ec4
Henri Biestro <henrib@apache.org>
2017-12-04 11:32:54 +0000

    JEXL-245: Better handling of null properties during de-referencing
    git-svn-id: https://svn.apache.org/repos/asf/commons/proper/jexl/trunk@1817082 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/commons/jexl3/scripting/JexlScriptEngineOptionalTest.java
+++ src/test/java/org/apache/commons/jexl3/scripting/JexlScriptEngineOptionalTest.java
@@ -24,37 +24,41 @@ import javax.script.CompiledScript;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
-import junit.framework.TestCase;
+import org.junit.Assert;
+import org.junit.Test;
 
-public class JexlScriptEngineOptionalTest extends TestCase {
+public class JexlScriptEngineOptionalTest {
     private final JexlScriptEngineFactory factory = new JexlScriptEngineFactory();
     private final ScriptEngineManager manager = new ScriptEngineManager();
     private final ScriptEngine engine = manager.getEngineByName("jexl");
 
+    @Test
     public void testOutput() throws Exception {
         String output = factory.getOutputStatement("foo\u00a9bar");
-        assertEquals("JEXL.out.print('foo\\u00a9bar')", output);
+        Assert.assertEquals("JEXL.out.print('foo\\u00a9bar')", output);
         // redirect output to capture evaluation result
         final StringWriter outContent = new StringWriter();
         engine.getContext().setWriter(outContent);
         engine.eval(output);
-        assertEquals("foo\u00a9bar", outContent.toString());
+        Assert.assertEquals("foo\u00a9bar", outContent.toString());
     }
 
+    @Test
     public void testError() throws Exception {
         String error = "JEXL.err.print('ERROR')";
         // redirect error to capture evaluation result
         final StringWriter outContent = new StringWriter();
         engine.getContext().setErrorWriter(outContent);
         engine.eval(error);
-        assertEquals("ERROR", outContent.toString());
+        Assert.assertEquals("ERROR", outContent.toString());
     }
 
+    @Test
     public void testCompilable() throws Exception {
-        assertTrue("Engine should implement Compilable", engine instanceof Compilable);
+        Assert.assertTrue("Engine should implement Compilable", engine instanceof Compilable);
         Compilable cengine = (Compilable) engine;
         CompiledScript script = cengine.compile("40 + 2");
-        assertEquals(Integer.valueOf(42), script.eval());
-        assertEquals(Integer.valueOf(42), script.eval());
+        Assert.assertEquals(Integer.valueOf(42), script.eval());
+        Assert.assertEquals(Integer.valueOf(42), script.eval());
     }
 }
