51902363b595264035486494611fb26d7c427185
Gary Gregory <garydgregory@gmail.com>
2020-12-20 13:20:40 -0500

    Fix spelling in Javadoc.

--- src/main/java/org/apache/commons/jexl3/internal/Engine.java
+++ src/main/java/org/apache/commons/jexl3/internal/Engine.java
@@ -877,7 +877,7 @@ public class Engine extends JexlEngine {
     /**
      * Trims the source from front and ending spaces.
      * @param str expression to clean
-     * @return trimmed expression ending in a semi-colon
+     * @return trimmed expression ending in a semicolon
      */
     protected String trimSource(final CharSequence str) {
         if (str != null) {
