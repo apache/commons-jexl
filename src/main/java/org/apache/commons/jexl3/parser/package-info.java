09f8bed43bb7d03ebd6828b57adf3b9eb532a386
henrib <henrib@apache.org>
2021-06-29 11:21:02 +0200

    JEXL: post 3.2.1 clean-up

---
+++ src/main/java/org/apache/commons/jexl3/parser/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * <p>
+ * Contains the Parser for JEXL script.
+ * </p>
+ * <p>
+ * This internal package is not intended for public usage and there is <b>no</b>
+ * guarantee that its public classes or methods will remain as is in subsequent
+ * versions.
+ * </p>
+ */
+package org.apache.commons.jexl3.parser;
