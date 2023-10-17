74490278f03e8e46a43ee275185f3bfcb5b73d86
Sebb <sebb@apache.org>
2023-10-17 21:35:09 +0100

    Simpler fix

--- src/main/java/org/apache/commons/jexl3/introspection/JexlPermissions.java
+++ src/main/java/org/apache/commons/jexl3/introspection/JexlPermissions.java
@@ -204,7 +204,7 @@ public interface JexlPermissions {
      * <p>This enables any public class, method, constructor or field to be visible to JEXL and used in scripts.</p>
      * @since 3.3
      */
-    JexlPermissions UNRESTRICTED = JexlPermissions.parse((String []) null);
+    JexlPermissions UNRESTRICTED = JexlPermissions.parse();
     /**
      * A restricted singleton.
      * <p>The RESTRICTED set is built using the following allowed packages and denied packages/classes.</p>
