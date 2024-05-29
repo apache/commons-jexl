d3c2bab6ea2af8180031dca0e6356f0e287f04d7
Gary Gregory <garydgregory@gmail.com>
2024-05-29 10:21:54 -0400

    Javadoc

--- src/main/java/org/apache/commons/jexl3/parser/ParseException.java
+++ src/main/java/org/apache/commons/jexl3/parser/ParseException.java
@@ -62,12 +62,18 @@ public class ParseException extends Exception implements JavaccError {
     }
 
     /**
-     * Default ctor.
+     * Constructs a new exception with {@code null} as its detail message. The cause is not initialized, and may subsequently be initialized by a call to
+     * {@link #initCause}.
      */
     public ParseException() {
     }
 
-    /** Constructor with message. */
+    /**
+     * Constructs a new exception with the specified detail message. The cause is not initialized, and may subsequently be initialized by a call to
+     * {@link #initCause}.
+     *
+     * @param message the detail message. The detail message is saved for later retrieval by the {@link #getMessage()} method.
+     */
     public ParseException(final String message) {
         super(message);
     }
