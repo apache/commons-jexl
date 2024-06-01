04e3899da30400795f16ee69cff226ab3a6a32d9
Gary Gregory <garydgregory@gmail.com>
2024-06-01 10:57:53 -0400

    Remove noop

--- src/test/java/org/apache/commons/jexl3/scripting/JexlScriptEngineTest.java
+++ src/test/java/org/apache/commons/jexl3/scripting/JexlScriptEngineTest.java
@@ -39,7 +39,6 @@ import org.apache.commons.jexl3.JexlBuilder;
 import org.apache.commons.jexl3.JexlException;
 import org.apache.commons.jexl3.introspection.JexlPermissions;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class JexlScriptEngineTest {
