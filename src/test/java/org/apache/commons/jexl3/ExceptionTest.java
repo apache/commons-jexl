90410bd4c9a8fae7066795f42490e79b6c75c3cd
Gary Gregory <garydgregory@gmail.com>
2024-06-01 09:18:20 -0400

    Port from JUnit 4 to 5

--- src/test/java/org/apache/commons/jexl3/ExceptionTest.java
+++ src/test/java/org/apache/commons/jexl3/ExceptionTest.java
@@ -16,10 +16,13 @@
  */
 package org.apache.commons.jexl3;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.apache.commons.jexl3.internal.Engine;
-import org.junit.Assert;
 import org.junit.Test;
 
 /**
