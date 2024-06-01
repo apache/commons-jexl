7981eecb9a83a4e40e65a0c6193e835c08c7a21e
Gary Gregory <garydgregory@gmail.com>
2024-06-01 08:11:10 -0400

    Sort members in AB order

--- src/main/java/org/apache/commons/jexl3/internal/introspection/AbstractExecutor.java
+++ src/main/java/org/apache/commons/jexl3/internal/introspection/AbstractExecutor.java
@@ -29,23 +29,65 @@ import org.apache.commons.jexl3.introspection.JexlPropertySet;
  * @since 1.0
  */
 abstract class AbstractExecutor {
-    /** A marker for invocation failures in tryInvoke. */
-    public static final Object TRY_FAILED = JexlEngine.TRY_FAILED;
-
     /**
-     * A helper to initialize the marker methods (array.get, list.get, etc...).
-     * @param clazz the class to introspect
-     * @param name the name of the method
-     * @param parms the parameters
-     * @return the method
+     * Abstract class that is used to execute an arbitrary 'get' method.
      */
-    static java.lang.reflect.Method initMarker(final Class<?> clazz, final String name, final Class<?>... parms) {
-        try {
-            return clazz.getMethod(name, parms);
-        } catch (final Exception xnever) {
-            throw new Error(xnever);
+    public abstract static class Get extends AbstractExecutor implements JexlPropertyGet {
+        /**
+         * Default and sole constructor.
+         * @param theClass the class this executor applies to
+         * @param theMethod the method held by this executor
+         */
+        protected Get(final Class<?> theClass, final java.lang.reflect.Method theMethod) {
+            super(theClass, theMethod);
         }
     }
+
+    /**
+     * Abstract class that is used to execute an arbitrary method.
+     */
+    public abstract static class Method extends AbstractExecutor implements JexlMethod {
+        /** The method key discovered from the arguments. */
+        protected final MethodKey key;
+
+        /**
+         * Creates a new instance.
+         * @param c the class this executor applies to
+         * @param m the method
+         * @param k the MethodKey
+         */
+        protected Method(final Class<?> c, final java.lang.reflect.Method m, final MethodKey k) {
+            super(c, m);
+            key = k;
+        }
+
+        @Override
+        public final Class<?> getReturnType() {
+            return method.getReturnType();
+        }
+
+        @Override
+        public Object getTargetProperty() {
+            return key;
+        }
+    }
+
+    /**
+     * Abstract class that is used to execute an arbitrary 'set' method.
+     */
+    public abstract static class Set extends AbstractExecutor implements JexlPropertySet {
+        /**
+         * Default and sole constructor.
+         * @param theClass the class this executor applies to
+         * @param theMethod the method held by this executor
+         */
+        protected Set(final Class<?> theClass, final java.lang.reflect.Method theMethod) {
+            super(theClass, theMethod);
+        }
+    }
+
+    /** A marker for invocation failures in tryInvoke. */
+    public static final Object TRY_FAILED = JexlEngine.TRY_FAILED;
 
     /**
      * Coerce an Object which must be a number to an Integer.
@@ -66,6 +108,29 @@ abstract class AbstractExecutor {
     }
 
     /**
+     * Gets the class of an object or Object if null.
+     * @param instance the instance
+     * @return the class
+     */
+    static Class<?> classOf(final Object instance) {
+        return instance == null ? Object.class : instance.getClass();
+    }
+    /**
+     * A helper to initialize the marker methods (array.get, list.get, etc...).
+     * @param clazz the class to introspect
+     * @param name the name of the method
+     * @param parms the parameters
+     * @return the method
+     */
+    static java.lang.reflect.Method initMarker(final Class<?> clazz, final String name, final Class<?>... parms) {
+        try {
+            return clazz.getMethod(name, parms);
+        } catch (final Exception xnever) {
+            throw new Error(xnever);
+        }
+    }
+
+    /**
      * Creates an arguments array.
      * @param args the list of arguments
      * @return the arguments array
@@ -74,17 +139,9 @@ abstract class AbstractExecutor {
         return args;
     }
 
-    /**
-     * Gets the class of an object or Object if null.
-     * @param instance the instance
-     * @return the class
-     */
-    static Class<?> classOf(final Object instance) {
-        return instance == null ? Object.class : instance.getClass();
-    }
-
     /** The class this executor applies to. */
     protected final Class<?> objectClass;
+
     /** Method to be executed. */
     protected final java.lang.reflect.Method method;
 
@@ -96,16 +153,6 @@ abstract class AbstractExecutor {
     protected AbstractExecutor(final Class<?> theClass, final java.lang.reflect.Method theMethod) {
         objectClass = theClass;
         method = theMethod;
-    }
-
-    @Override
-    public boolean equals(final Object obj) {
-        return this == obj || obj instanceof AbstractExecutor && equals((AbstractExecutor) obj);
-    }
-
-    @Override
-    public int hashCode() {
-        return method.hashCode();
     }
 
     /**
@@ -136,6 +183,48 @@ abstract class AbstractExecutor {
         return false;
     }
 
+    @Override
+    public boolean equals(final Object obj) {
+        return this == obj || obj instanceof AbstractExecutor && equals((AbstractExecutor) obj);
+    }
+
+    /**
+     * Gets the method to be executed or used as a marker.
+     * @return Method The method used by execute in derived classes.
+     */
+    public final java.lang.reflect.Method getMethod() {
+        return method;
+    }
+
+    /**
+     * Gets the method name used.
+     * @return method name
+     */
+    public final String getMethodName() {
+        return method.getName();
+    }
+
+    /**
+     * Gets the object class targeted by this executor.
+     * @return the target object class
+     */
+    public final Class<?> getTargetClass() {
+        return objectClass;
+    }
+
+    /**
+     * Gets the property targeted by this executor.
+     * @return the target property
+     */
+    public Object getTargetProperty() {
+        return null;
+    }
+
+    @Override
+    public int hashCode() {
+        return method.hashCode();
+    }
+
     /**
      * Tell whether the executor is alive by looking
      * at the value of the method.
@@ -157,100 +246,11 @@ abstract class AbstractExecutor {
     }
 
     /**
-     * Gets the method to be executed or used as a marker.
-     * @return Method The method used by execute in derived classes.
-     */
-    public final java.lang.reflect.Method getMethod() {
-        return method;
-    }
-
-    /**
-     * Gets the object class targeted by this executor.
-     * @return the target object class
-     */
-    public final Class<?> getTargetClass() {
-        return objectClass;
-    }
-
-    /**
-     * Gets the property targeted by this executor.
-     * @return the target property
-     */
-    public Object getTargetProperty() {
-        return null;
-    }
-
-    /**
-     * Gets the method name used.
-     * @return method name
-     */
-    public final String getMethodName() {
-        return method.getName();
-    }
-
-    /**
      * Checks whether a tryExecute failed or not.
      * @param exec the value returned by tryExecute
      * @return true if tryExecute failed, false otherwise
      */
     public final boolean tryFailed(final Object exec) {
         return exec == JexlEngine.TRY_FAILED;
-    }
-
-    /**
-     * Abstract class that is used to execute an arbitrary 'get' method.
-     */
-    public abstract static class Get extends AbstractExecutor implements JexlPropertyGet {
-        /**
-         * Default and sole constructor.
-         * @param theClass the class this executor applies to
-         * @param theMethod the method held by this executor
-         */
-        protected Get(final Class<?> theClass, final java.lang.reflect.Method theMethod) {
-            super(theClass, theMethod);
-        }
-    }
-
-    /**
-     * Abstract class that is used to execute an arbitrary 'set' method.
-     */
-    public abstract static class Set extends AbstractExecutor implements JexlPropertySet {
-        /**
-         * Default and sole constructor.
-         * @param theClass the class this executor applies to
-         * @param theMethod the method held by this executor
-         */
-        protected Set(final Class<?> theClass, final java.lang.reflect.Method theMethod) {
-            super(theClass, theMethod);
-        }
-    }
-
-    /**
-     * Abstract class that is used to execute an arbitrary method.
-     */
-    public abstract static class Method extends AbstractExecutor implements JexlMethod {
-        /** The method key discovered from the arguments. */
-        protected final MethodKey key;
-
-        /**
-         * Creates a new instance.
-         * @param c the class this executor applies to
-         * @param m the method
-         * @param k the MethodKey
-         */
-        protected Method(final Class<?> c, final java.lang.reflect.Method m, final MethodKey k) {
-            super(c, m);
-            key = k;
-        }
-
-        @Override
-        public Object getTargetProperty() {
-            return key;
-        }
-
-        @Override
-        public final Class<?> getReturnType() {
-            return method.getReturnType();
-        }
     }
 }
