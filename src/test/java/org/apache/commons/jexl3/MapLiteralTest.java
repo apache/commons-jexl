eff67027555d7717fd7b1f21048af79eab65d403
Gary Gregory <garydgregory@gmail.com>
2024-06-01 09:18:32 -0400

    Port from JUnit 4 to 5

--- src/test/java/org/apache/commons/jexl3/MapLiteralTest.java
+++ src/test/java/org/apache/commons/jexl3/MapLiteralTest.java
@@ -16,7 +16,11 @@
  */
 package org.apache.commons.jexl3;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -24,7 +28,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 /**
