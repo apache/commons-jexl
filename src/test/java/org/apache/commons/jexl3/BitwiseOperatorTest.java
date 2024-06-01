7e110435b50d3a8249bc93bce49ca22443bb48ef
Gary Gregory <garydgregory@gmail.com>
2024-06-01 09:18:13 -0400

    Port from JUnit 4 to 5

--- src/test/java/org/apache/commons/jexl3/BitwiseOperatorTest.java
+++ src/test/java/org/apache/commons/jexl3/BitwiseOperatorTest.java
@@ -16,8 +16,6 @@
  */
 package org.apache.commons.jexl3;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.apache.commons.jexl3.junit.Asserter;
 import org.junit.Before;
 import org.junit.Test;
