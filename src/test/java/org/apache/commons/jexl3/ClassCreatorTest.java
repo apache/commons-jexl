0f5e13b0f79278acd703289db860bef3f744535a
Gary Gregory <garydgregory@gmail.com>
2024-06-01 09:18:17 -0400

    Port from JUnit 4 to 5

--- src/test/java/org/apache/commons/jexl3/ClassCreatorTest.java
+++ src/test/java/org/apache/commons/jexl3/ClassCreatorTest.java
@@ -16,7 +16,11 @@
  */
 package org.apache.commons.jexl3;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNotSame;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
 
 import java.io.File;
 import java.lang.ref.Reference;
@@ -32,7 +36,6 @@ import org.apache.commons.lang3.SystemProperties;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
