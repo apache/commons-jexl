5c291e7cbad76613a0063bbd53402375577f8657
Gary Gregory <garydgregory@gmail.com>
2024-06-01 11:09:21 -0400

    Use assertThrows()

--- src/test/java/org/apache/commons/jexl3/internal/introspection/PermissionsTest.java
+++ src/test/java/org/apache/commons/jexl3/internal/introspection/PermissionsTest.java
@@ -23,7 +23,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
