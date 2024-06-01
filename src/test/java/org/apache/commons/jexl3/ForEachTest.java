697d261caee3a386d2a3516fa237dd1351c62f8a
Gary Gregory <garydgregory@gmail.com>
2024-06-01 09:18:22 -0400

    Port from JUnit 4 to 5

--- src/test/java/org/apache/commons/jexl3/ForEachTest.java
+++ src/test/java/org/apache/commons/jexl3/ForEachTest.java
@@ -16,7 +16,11 @@
  */
 package org.apache.commons.jexl3;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -25,7 +29,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.StringTokenizer;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 /**
