17d7d8754227d314f89463240da9ddf0b04f5adf
Gary Gregory <garydgregory@gmail.com>
2024-06-01 15:10:52 -0400

    Clean up imports

--- src/test/java/org/apache/commons/jexl3/ForEachTest.java
+++ src/test/java/org/apache/commons/jexl3/ForEachTest.java
@@ -21,6 +21,7 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
