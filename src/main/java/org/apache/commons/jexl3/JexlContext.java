c50e076ec04afed47736cdc380c818b60e2b69ab
Gary Gregory <garydgregory@gmail.com>
2024-06-01 08:10:43 -0400

    Sort members in AB order

--- src/main/java/org/apache/commons/jexl3/JexlContext.java
+++ src/main/java/org/apache/commons/jexl3/JexlContext.java
@@ -40,101 +40,6 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public interface JexlContext {
 
     /**
-     * Gets the value of a variable.
-     *
-     * @param name the variable's name
-     * @return the value
-     */
-    Object get(String name);
-
-    /**
-     * Sets the value of a variable.
-     *
-     * @param name the variable's name
-     * @param value the variable's value
-     */
-    void set(String name, Object value);
-
-    /**
-     * Checks whether a variable is defined in this context.
-     *
-     * <p>A variable may be defined with a null value; this method checks whether the
-     * value is null or if the variable is undefined.</p>
-     *
-     * @param name the variable's name
-     * @return true if it exists, false otherwise
-     */
-    boolean has(String name);
-
-    /**
-     * A marker interface of the JexlContext that declares how to resolve a namespace from its name;
-     * it is used by the interpreter during evaluation.
-     *
-     * <p>In JEXL, a namespace is an object that serves the purpose of encapsulating functions; for instance,
-     * the "math" namespace would be the proper object to expose functions like "log(...)", "sinus(...)", etc.</p>
-     *
-     * In expressions like "ns:function(...)", the resolver is called with resolveNamespace("ns").
-     *
-     * <p>JEXL itself reserves 'jexl' and 'ujexl' as namespaces for internal purpose; resolving those may lead to
-     * unexpected results.</p>
-     *
-     * @since 3.0
-     */
-    interface NamespaceResolver {
-
-        /**
-         * Resolves a namespace by its name.
-         * @param name the name
-         * @return the namespace object
-         */
-        Object resolveNamespace(String name);
-    }
-
-    /**
-     * A marker interface that solves a simple class name into a fully-qualified one.
-     * @since 3.3
-     */
-    interface ClassNameResolver {
-        /**
-         * Resolves a class name.
-         * @param name the simple class name
-         * @return the fully qualified class name
-         */
-        String resolveClassName(String name);
-    }
-
-    /**
-     * A marker interface of the JexlContext, NamespaceFunctor allows creating an instance
-     * to delegate namespace methods calls to.
-     *
-     * <p>The functor is created once during the lifetime of a script evaluation.</p>
-     */
-    interface NamespaceFunctor {
-        /**
-         * Creates the functor object that will be used instead of the namespace.
-         * @param context the context
-         * @return the namespace functor instance
-         */
-        Object createFunctor(JexlContext context);
-    }
-
-    /**
-     * A marker interface  of the JexlContext that indicates the interpreter to put this context
-     * in the JexlEngine thread local context instance during evaluation.
-     * This allows user functions or methods to access the context during a call.
-     * Note that the usual caveats wrt using thread local apply (caching/leaking references, etc.); in particular,
-     * keeping a reference to such a context is to be considered with great care and caution.
-     * It should also be noted that sharing such a context between threads should implicate synchronizing variable
-     * accessing the implementation class.
-     *
-     * @see JexlEngine#setThreadContext(JexlContext.ThreadLocal)
-     * @see JexlEngine#getThreadContext()
-     */
-    interface ThreadLocal extends JexlContext {
-        // no specific method
-    }
-
-    /**
      * A marker interface of the JexlContext that processes annotations.
      * It is used by the interpreter during evaluation to execute annotation evaluations.
      * <p>If the JexlContext is not an instance of an AnnotationProcessor, encountering an annotation will generate
@@ -159,6 +64,34 @@ public interface JexlContext {
     }
 
     /**
+     * A marker interface of the JexlContext sharing a cancelling flag.
+     * <p>A script running in a thread can thus be notified through this reference
+     * of its cancellation through the context. It uses the same interpreter logic
+     * that reacts to cancellation and is an alternative to using callable() and/or
+     * interrupting script interpreter threads.
+     * @since 3.2
+     */
+    interface CancellationHandle {
+        /**
+         * @return a cancelable boolean used by the interpreter
+         */
+        AtomicBoolean getCancellation();
+    }
+
+    /**
+     * A marker interface that solves a simple class name into a fully-qualified one.
+     * @since 3.3
+     */
+    interface ClassNameResolver {
+        /**
+         * Resolves a class name.
+         * @param name the simple class name
+         * @return the fully qualified class name
+         */
+        String resolveClassName(String name);
+    }
+
+    /**
      * A marker interface of the JexlContext that processes module definitions.
      * It is used by the interpreter during evaluation of the pragma module definitions.
      * @since 3.3
@@ -175,6 +108,45 @@ public interface JexlContext {
          * @return the module object
          */
         Object processModule(JexlEngine engine, JexlInfo info, String name, String body);
+    }
+
+    /**
+     * A marker interface of the JexlContext, NamespaceFunctor allows creating an instance
+     * to delegate namespace methods calls to.
+     *
+     * <p>The functor is created once during the lifetime of a script evaluation.</p>
+     */
+    interface NamespaceFunctor {
+        /**
+         * Creates the functor object that will be used instead of the namespace.
+         * @param context the context
+         * @return the namespace functor instance
+         */
+        Object createFunctor(JexlContext context);
+    }
+
+    /**
+     * A marker interface of the JexlContext that declares how to resolve a namespace from its name;
+     * it is used by the interpreter during evaluation.
+     *
+     * <p>In JEXL, a namespace is an object that serves the purpose of encapsulating functions; for instance,
+     * the "math" namespace would be the proper object to expose functions like "log(...)", "sinus(...)", etc.</p>
+     *
+     * In expressions like "ns:function(...)", the resolver is called with resolveNamespace("ns").
+     *
+     * <p>JEXL itself reserves 'jexl' and 'ujexl' as namespaces for internal purpose; resolving those may lead to
+     * unexpected results.</p>
+     *
+     * @since 3.0
+     */
+    interface NamespaceResolver {
+
+        /**
+         * Resolves a namespace by its name.
+         * @param name the name
+         * @return the namespace object
+         */
+        Object resolveNamespace(String name);
     }
 
     /**
@@ -204,6 +176,17 @@ public interface JexlContext {
     interface PragmaProcessor {
         /**
          * Process one pragma.
+         * @param opts the current evaluator options
+         * @param key the key
+         * @param value the value
+         * @since 3.3
+         */
+        default void processPragma(final JexlOptions opts, final String key, final Object value) {
+            processPragma(key, value);
+        }
+
+        /**
+         * Process one pragma.
          * <p>Never called in 3.3, must be implemented for 3.2 binary compatibility reasons.</p>
          * <p>Typical implementation in 3.3:</p>
          * <code>
@@ -218,31 +201,48 @@ public interface JexlContext {
          */
         @Deprecated
         void processPragma(String key, Object value);
-
-        /**
-         * Process one pragma.
-         * @param opts the current evaluator options
-         * @param key the key
-         * @param value the value
-         * @since 3.3
-         */
-        default void processPragma(final JexlOptions opts, final String key, final Object value) {
-            processPragma(key, value);
-        }
     }
 
     /**
-     * A marker interface of the JexlContext sharing a cancelling flag.
-     * <p>A script running in a thread can thus be notified through this reference
-     * of its cancellation through the context. It uses the same interpreter logic
-     * that reacts to cancellation and is an alternative to using callable() and/or
-     * interrupting script interpreter threads.
-     * @since 3.2
+     * A marker interface  of the JexlContext that indicates the interpreter to put this context
+     * in the JexlEngine thread local context instance during evaluation.
+     * This allows user functions or methods to access the context during a call.
+     * Note that the usual caveats wrt using thread local apply (caching/leaking references, etc.); in particular,
+     * keeping a reference to such a context is to be considered with great care and caution.
+     * It should also be noted that sharing such a context between threads should implicate synchronizing variable
+     * accessing the implementation class.
+     *
+     * @see JexlEngine#setThreadContext(JexlContext.ThreadLocal)
+     * @see JexlEngine#getThreadContext()
      */
-    interface CancellationHandle {
-        /**
-         * @return a cancelable boolean used by the interpreter
-         */
-        AtomicBoolean getCancellation();
+    interface ThreadLocal extends JexlContext {
+        // no specific method
     }
+
+    /**
+     * Gets the value of a variable.
+     *
+     * @param name the variable's name
+     * @return the value
+     */
+    Object get(String name);
+
+    /**
+     * Checks whether a variable is defined in this context.
+     *
+     * <p>A variable may be defined with a null value; this method checks whether the
+     * value is null or if the variable is undefined.</p>
+     *
+     * @param name the variable's name
+     * @return true if it exists, false otherwise
+     */
+    boolean has(String name);
+
+    /**
+     * Sets the value of a variable.
+     *
+     * @param name the variable's name
+     * @param value the variable's value
+     */
+    void set(String name, Object value);
 }
