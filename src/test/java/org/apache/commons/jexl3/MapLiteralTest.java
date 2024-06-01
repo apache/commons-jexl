a47fe38a42ff2cb80bc5f03f39933497b283bbd1
Gary Gregory <garydgregory@gmail.com>
2024-06-01 18:54:10 -0400

    Use assertThrows()

--- src/test/java/org/apache/commons/jexl3/MapLiteralTest.java
+++ src/test/java/org/apache/commons/jexl3/MapLiteralTest.java
@@ -21,7 +21,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.Arrays;
 import java.util.Collections;
