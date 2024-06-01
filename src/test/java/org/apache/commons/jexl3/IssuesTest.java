b3838c776e9fc482d7acc2e1e750d13d22ec5f7f
Gary Gregory <garydgregory@gmail.com>
2024-06-01 18:16:06 -0400

    Clean up imports

--- src/test/java/org/apache/commons/jexl3/IssuesTest.java
+++ src/test/java/org/apache/commons/jexl3/IssuesTest.java
@@ -22,7 +22,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.fail;
 
 import java.util.HashMap;
 import java.util.Map;
