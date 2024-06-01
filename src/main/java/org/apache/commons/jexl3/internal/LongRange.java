3e85b8816daf230df5654a9223d8b6287b3318fa
Gary Gregory <garydgregory@gmail.com>
2024-06-01 08:11:01 -0400

    Sort members in AB order

--- src/main/java/org/apache/commons/jexl3/internal/LongRange.java
+++ src/main/java/org/apache/commons/jexl3/internal/LongRange.java
@@ -22,216 +22,6 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 /**
- * A range of longs.
- * <p>
- * Behaves as a readonly collection of longs.
- */
-public abstract class LongRange implements Collection<Long> {
-    /** The lower boundary. */
-    protected final long min;
-    /** The upper boundary. */
-    protected final long max;
-
-    /**
-     * Creates a range, ascending or descending depending on boundaries order.
-     * @param from the lower inclusive boundary
-     * @param to   the higher inclusive boundary
-     * @return a range
-     */
-    public static LongRange create(final long from, final long to) {
-        if (from <= to) {
-            return new LongRange.Ascending(from, to);
-        }
-        return new LongRange.Descending(to, from);
-    }
-
-    /**
-     * Creates a new range.
-     * @param from the lower inclusive boundary
-     * @param to   the higher inclusive boundary
-     */
-    protected LongRange(final long from, final long to) {
-        min = from;
-        max = to;
-    }
-
-    /**
-     * Gets the interval minimum value.
-     * @return the low boundary
-     */
-    public long getMin() {
-        return min;
-    }
-
-    /**
-     * Gets the interval maximum value.
-     * @return the high boundary
-     */
-    public long getMax() {
-        return max;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = getClass().hashCode();
-        //CSOFF: MagicNumber
-        hash = 13 * hash + (int) (this.min ^ this.min >>> 32);
-        hash = 13 * hash + (int) (this.max ^ this.max >>> 32);
-        //CSON: MagicNumber
-        return hash;
-    }
-
-    @Override
-    public boolean equals(final Object obj) {
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final LongRange other = (LongRange) obj;
-        if (this.min != other.min) {
-            return false;
-        }
-        if (this.max != other.max) {
-            return false;
-        }
-        return true;
-    }
-
-    @Override
-    public abstract Iterator<Long> iterator();
-
-    @Override
-    public int size() {
-        return (int) (max - min + 1);
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public boolean contains(final Object o) {
-        if (o instanceof Number) {
-            final long v = ((Number) o).longValue();
-            return min <= v && v <= max;
-        }
-        return false;
-    }
-
-    @Override
-    public Object[] toArray() {
-        final int size = size();
-        final Object[] array = new Object[size];
-        for (int a = 0; a < size; ++a) {
-            array[a] = min + a;
-        }
-        return array;
-    }
-
-    @Override
-    @SuppressWarnings("unchecked")
-    public <T> T[] toArray(final T[] array) {
-        final Class<?> ct = array.getClass().getComponentType();
-        final int length = size();
-        T[] copy = array;
-        if (ct.isAssignableFrom(Long.class)) {
-            if (array.length < length) {
-                copy = (T[]) Array.newInstance(ct, length);
-            }
-            for (int a = 0; a < length; ++a) {
-                Array.set(copy, a, min + a);
-            }
-            if (length < copy.length) {
-                copy[length] = null;
-            }
-            return copy;
-        }
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean containsAll(final Collection<?> c) {
-        for (final Object cc : c) {
-            if (!contains(cc)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public boolean add(final Long e) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean remove(final Object o) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean addAll(final Collection<? extends Long> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean removeAll(final Collection<?> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean retainAll(final Collection<?> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException();
-    }
-
-    /**
-     * Ascending long range.
-     */
-    public static class Ascending extends LongRange {
-        /**
-         * Constructs a new instance.
-         * @param from lower boundary
-         * @param to upper boundary
-         */
-        protected Ascending(final long from, final long to) {
-            super(from, to);
-        }
-
-        @Override
-        public Iterator<Long> iterator() {
-            return new AscLongIterator(min, max);
-        }
-    }
-
-    /**
-     * Descending long range.
-     */
-    public static class Descending extends LongRange {
-        /**
-         * Constructs a new instance.
-         * @param from upper boundary
-         * @param to lower boundary
-         */
-        protected Descending(final long from, final long to) {
-            super(from, to);
-        }
-
-        @Override
-        public Iterator<Long> iterator() {
-            return new DescLongIterator(min, max);
-        }
-    }
-}
-
-/**
  * An iterator on a long range.
  */
 final class AscLongIterator implements Iterator<Long> {
@@ -300,5 +90,215 @@ final class DescLongIterator implements Iterator<Long> {
             return cursor--;
         }
         throw new NoSuchElementException();
+    }
+}
+
+/**
+ * A range of longs.
+ * <p>
+ * Behaves as a readonly collection of longs.
+ */
+public abstract class LongRange implements Collection<Long> {
+    /**
+     * Ascending long range.
+     */
+    public static class Ascending extends LongRange {
+        /**
+         * Constructs a new instance.
+         * @param from lower boundary
+         * @param to upper boundary
+         */
+        protected Ascending(final long from, final long to) {
+            super(from, to);
+        }
+
+        @Override
+        public Iterator<Long> iterator() {
+            return new AscLongIterator(min, max);
+        }
+    }
+    /**
+     * Descending long range.
+     */
+    public static class Descending extends LongRange {
+        /**
+         * Constructs a new instance.
+         * @param from upper boundary
+         * @param to lower boundary
+         */
+        protected Descending(final long from, final long to) {
+            super(from, to);
+        }
+
+        @Override
+        public Iterator<Long> iterator() {
+            return new DescLongIterator(min, max);
+        }
+    }
+
+    /**
+     * Creates a range, ascending or descending depending on boundaries order.
+     * @param from the lower inclusive boundary
+     * @param to   the higher inclusive boundary
+     * @return a range
+     */
+    public static LongRange create(final long from, final long to) {
+        if (from <= to) {
+            return new LongRange.Ascending(from, to);
+        }
+        return new LongRange.Descending(to, from);
+    }
+
+    /** The lower boundary. */
+    protected final long min;
+
+    /** The upper boundary. */
+    protected final long max;
+
+    /**
+     * Creates a new range.
+     * @param from the lower inclusive boundary
+     * @param to   the higher inclusive boundary
+     */
+    protected LongRange(final long from, final long to) {
+        min = from;
+        max = to;
+    }
+
+    @Override
+    public boolean add(final Long e) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean addAll(final Collection<? extends Long> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean contains(final Object o) {
+        if (o instanceof Number) {
+            final long v = ((Number) o).longValue();
+            return min <= v && v <= max;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(final Collection<?> c) {
+        for (final Object cc : c) {
+            if (!contains(cc)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final LongRange other = (LongRange) obj;
+        if (this.min != other.min) {
+            return false;
+        }
+        if (this.max != other.max) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Gets the interval maximum value.
+     * @return the high boundary
+     */
+    public long getMax() {
+        return max;
+    }
+
+    /**
+     * Gets the interval minimum value.
+     * @return the low boundary
+     */
+    public long getMin() {
+        return min;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = getClass().hashCode();
+        //CSOFF: MagicNumber
+        hash = 13 * hash + (int) (this.min ^ this.min >>> 32);
+        hash = 13 * hash + (int) (this.max ^ this.max >>> 32);
+        //CSON: MagicNumber
+        return hash;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public abstract Iterator<Long> iterator();
+
+    @Override
+    public boolean remove(final Object o) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean removeAll(final Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public boolean retainAll(final Collection<?> c) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public int size() {
+        return (int) (max - min + 1);
+    }
+
+    @Override
+    public Object[] toArray() {
+        final int size = size();
+        final Object[] array = new Object[size];
+        for (int a = 0; a < size; ++a) {
+            array[a] = min + a;
+        }
+        return array;
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public <T> T[] toArray(final T[] array) {
+        final Class<?> ct = array.getClass().getComponentType();
+        final int length = size();
+        T[] copy = array;
+        if (ct.isAssignableFrom(Long.class)) {
+            if (array.length < length) {
+                copy = (T[]) Array.newInstance(ct, length);
+            }
+            for (int a = 0; a < length; ++a) {
+                Array.set(copy, a, min + a);
+            }
+            if (length < copy.length) {
+                copy[length] = null;
+            }
+            return copy;
+        }
+        throw new UnsupportedOperationException();
     }
 }
