43bdae318080b14bd85ca8a78ac7fc46a03a3ed8
Gary Gregory <garydgregory@gmail.com>
2024-06-01 09:18:48 -0400

    Port from JUnit 4 to 5

--- src/test/java/org/apache/commons/jexl3/examples/MethodPropertyTest.java
+++ src/test/java/org/apache/commons/jexl3/examples/MethodPropertyTest.java
@@ -17,8 +17,6 @@
 
 package org.apache.commons.jexl3.examples;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.apache.commons.jexl3.JexlBuilder;
 import org.apache.commons.jexl3.JexlContext;
 import org.apache.commons.jexl3.JexlEngine;
