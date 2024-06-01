6709ca49adc7feb0a73e6dd5160f39500a7d843f
Gary Gregory <garydgregory@gmail.com>
2024-06-01 08:11:37 -0400

    Sort members in AB order

--- src/main/java/org/apache/commons/jexl3/parser/ASTIdentifier.java
+++ src/main/java/org/apache/commons/jexl3/parser/ASTIdentifier.java
@@ -23,46 +23,26 @@ public class ASTIdentifier extends JexlNode {
     /**
      */
     private static final long serialVersionUID = 1L;
-    protected String name;
-    protected int symbol = -1;
-    protected int flags;
-
     /** The redefined variable flag. */
     private static final int REDEFINED = 0;
     /** The shaded variable flag. */
     private static final int SHADED = 1;
     /** The captured variable flag. */
     private static final int CAPTURED = 2;
+
     /** The lexical variable flag. */
     private static final int LEXICAL = 3;
     /** The const variable flag. */
     private static final int CONST = 4;
-
-    ASTIdentifier(final int id) {
-        super(id);
+    /**
+     * Checks the value of a flag in the mask.
+     * @param ordinal the flag ordinal
+     * @param mask the flags mask
+     * @return the mask value with this flag or-ed in
+     */
+    private static boolean isSet(final int ordinal, final int mask) {
+        return (mask & 1 << ordinal) != 0;
     }
-
-    @Override
-    public String toString() {
-        return name;
-    }
-
-    void setSymbol(final String identifier) {
-        if (identifier.charAt(0) == '#') {
-            symbol = Integer.parseInt(identifier.substring(1));
-        }
-        name = identifier;
-    }
-
-    void setSymbol(final int r, final String identifier) {
-        symbol = r;
-        name = identifier;
-    }
-
-    public int getSymbol() {
-        return symbol;
-    }
-
     /**
      * Sets the value of a flag in a mask.
      * @param ordinal the flag ordinal
@@ -73,56 +53,14 @@ public class ASTIdentifier extends JexlNode {
     private static int set(final int ordinal, final int mask, final boolean value) {
         return value? mask | 1 << ordinal : mask & ~(1 << ordinal);
     }
+    protected String name;
 
-    /**
-     * Checks the value of a flag in the mask.
-     * @param ordinal the flag ordinal
-     * @param mask the flags mask
-     * @return the mask value with this flag or-ed in
-     */
-    private static boolean isSet(final int ordinal, final int mask) {
-        return (mask & 1 << ordinal) != 0;
-    }
+    protected int symbol = -1;
 
-    public void setRedefined(final boolean f) {
-        flags = set(REDEFINED, flags, f);
-    }
+    protected int flags;
 
-    public boolean isRedefined() {
-        return isSet(REDEFINED, flags);
-    }
-
-    public void setShaded(final boolean f) {
-        flags = set(SHADED, flags, f);
-    }
-
-    public boolean isShaded() {
-        return isSet(SHADED, flags);
-    }
-
-    public void setCaptured(final boolean f) {
-        flags = set(CAPTURED, flags, f);
-    }
-
-    public boolean isCaptured() {
-        return isSet(CAPTURED, flags);
-    }
-
-    public boolean isLexical() {
-        return isSet(LEXICAL, flags);
-    }
-
-    public void setLexical(final boolean f) {
-        flags = set(LEXICAL, flags, f);
-    }
-
-    @Override
-    public boolean isConstant() {
-        return isSet(CONST, flags);
-    }
-
-    public void setConstant(final boolean f) {
-        flags = set(CONST, flags, f);
+    ASTIdentifier(final int id) {
+        super(id);
     }
 
     public String getName() {
@@ -133,8 +71,70 @@ public class ASTIdentifier extends JexlNode {
         return null;
     }
 
+    public int getSymbol() {
+        return symbol;
+    }
+
+    public boolean isCaptured() {
+        return isSet(CAPTURED, flags);
+    }
+
+    @Override
+    public boolean isConstant() {
+        return isSet(CONST, flags);
+    }
+
+    public boolean isLexical() {
+        return isSet(LEXICAL, flags);
+    }
+
+    public boolean isRedefined() {
+        return isSet(REDEFINED, flags);
+    }
+
+    public boolean isShaded() {
+        return isSet(SHADED, flags);
+    }
+
     @Override
     public Object jjtAccept(final ParserVisitor visitor, final Object data) {
         return visitor.visit(this, data);
+    }
+
+    public void setCaptured(final boolean f) {
+        flags = set(CAPTURED, flags, f);
+    }
+
+    public void setConstant(final boolean f) {
+        flags = set(CONST, flags, f);
+    }
+
+    public void setLexical(final boolean f) {
+        flags = set(LEXICAL, flags, f);
+    }
+
+    public void setRedefined(final boolean f) {
+        flags = set(REDEFINED, flags, f);
+    }
+
+    public void setShaded(final boolean f) {
+        flags = set(SHADED, flags, f);
+    }
+
+    void setSymbol(final int r, final String identifier) {
+        symbol = r;
+        name = identifier;
+    }
+
+    void setSymbol(final String identifier) {
+        if (identifier.charAt(0) == '#') {
+            symbol = Integer.parseInt(identifier.substring(1));
+        }
+        name = identifier;
+    }
+
+    @Override
+    public String toString() {
+        return name;
     }
 }
