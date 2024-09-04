8aa46e4a6038a45a9b0341992acdde041455d851
Henrib <hbiestro@gmail.com>
2024-09-04 10:29:01 +0200

    JEXL: add test on using regexp array (SO question)

--- src/test/java/org/apache/commons/jexl3/Issues400Test.java
+++ src/test/java/org/apache/commons/jexl3/Issues400Test.java
@@ -28,7 +28,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.lang.reflect.Method;
 import java.math.BigDecimal;
-import java.math.MathContext;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
