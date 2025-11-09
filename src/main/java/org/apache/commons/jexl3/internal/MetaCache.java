4a2d4e4570ae33a1b374626f98d10ecae63cc062
Henrib <hbiestro@gmail.com>
2025-11-09 23:50:17 +0100

    JEXL-448: addressing #pr comments (space 4 indents, purpose clarifications);

--- src/main/java/org/apache/commons/jexl3/internal/MetaCache.java
+++ src/main/java/org/apache/commons/jexl3/internal/MetaCache.java
@@ -28,77 +28,77 @@ import org.apache.commons.jexl3.JexlCache;
 /**
  * A meta-cache that tracks multiple JexlCache instances via weak references.
  * <p>
- *  Each JexlCache created by this MetaCache is held via a WeakReference,
- *  allowing it to be garbage collected as soon as no strong references exist.
+ * Each JexlCache created by this MetaCache is held via a WeakReference,
+ * allowing it to be garbage collected as soon as no strong references exist.
  * </p>
  * <p>
  * This allows for collective management of multiple caches, in particular clearing all caches at once.
  * This operation is typically called when the uberspect class loader needs to change.
  * </p>
  */
-public class MetaCache {
-  // The factory to create new JexlCache instances
-  private final IntFunction<JexlCache<?, ?>> factory;
-  // The set of JexlCache references
-  private final Set<Reference<JexlCache<?,?>>> references;
-  // Queue to receive references whose referent has been garbage collected
-  private final ReferenceQueue<JexlCache<?,?>> queue;
+final class MetaCache {
+    // The factory to create new JexlCache instances
+    private final IntFunction<JexlCache<?, ?>> factory;
+    // The set of JexlCache references
+    private final Set<Reference<JexlCache<?, ?>>> references;
+    // Queue to receive references whose referent has been garbage collected
+    private final ReferenceQueue<JexlCache<?, ?>> queue;
 
-  public MetaCache(final IntFunction<JexlCache<?, ?>> factory) {
-    this.factory = factory;
-    this.references = new HashSet<>();
-    this.queue = new ReferenceQueue<>();
-  }
+    public MetaCache(final IntFunction<JexlCache<?, ?>> factory) {
+        this.factory = factory;
+        this.references = new HashSet<>();
+        this.queue = new ReferenceQueue<>();
+    }
 
-  @SuppressWarnings("unchecked")
-  public <K,V> JexlCache<K,V> createCache(final int capacity) {
-    if (capacity > 0) {
-      JexlCache<K, V> cache = (JexlCache<K, V>) factory.apply(capacity);
-      if (cache != null) {
-        synchronized(references) {
-          // The reference is created with the queue for automatic cleanup
-          references.add(new WeakReference<>(cache, queue));
-          // Always clean up the queue after modification to keep the set tidy
-          cleanUp();
+    @SuppressWarnings("unchecked")
+    public <K, V> JexlCache<K, V> createCache(final int capacity) {
+        if (capacity > 0) {
+            JexlCache<K, V> cache = (JexlCache<K, V>) factory.apply(capacity);
+            if (cache != null) {
+                synchronized (references) {
+                    // The reference is created with the queue for automatic cleanup
+                    references.add(new WeakReference<>(cache, queue));
+                    // Always clean up the queue after modification to keep the set tidy
+                    cleanUp();
+                }
+            }
+            return cache;
         }
-      }
-      return cache;
+        return null;
     }
-    return null;
-  }
 
-  public void clearCaches() {
-    synchronized (references) {
-      for (Reference<JexlCache<?,?>> ref : references) {
-        JexlCache<?,?> cache = ref.get();
-        if (cache != null) {
-          cache.clear();
+    public void clearCaches() {
+        synchronized (references) {
+            for (Reference<JexlCache<?, ?>> ref : references) {
+                JexlCache<?, ?> cache = ref.get();
+                if (cache != null) {
+                    cache.clear();
+                }
+            }
+            cleanUp();
         }
-      }
-      cleanUp();
     }
-  }
 
-  /**
-   * Cleans up all references whose referent (the cache) has been garbage collected.
-   * <p>This method must be invoked while holding the lock on {@code references}.</p>
-   *
-   * @return The remaining number of caches.
-   */
-  @SuppressWarnings("unchecked")
-  private int cleanUp() {
-      // The poll() method returns the next reference object in the queue, or null if none is available.
-      Reference<JexlCache<?,?>> reference;
-      while ((reference = (Reference<JexlCache<?, ?>>) queue.poll()) != null) {
-        // Remove the reference from the set
-        references.remove(reference);
-      }
-      return references.size();
-  }
-
-  public int size() {
-    synchronized (references) {
-      return cleanUp();
+    /**
+     * Cleans up all references whose referent (the cache) has been garbage collected.
+     * <p>This method must be invoked while holding the lock on {@code references}.</p>
+     *
+     * @return The remaining number of caches.
+     */
+    @SuppressWarnings("unchecked")
+    private int cleanUp() {
+        // The poll() method returns the next reference object in the queue, or null if none is available.
+        Reference<JexlCache<?, ?>> reference;
+        while ((reference = (Reference<JexlCache<?, ?>>) queue.poll()) != null) {
+            // Remove the reference from the set
+            references.remove(reference);
+        }
+        return references.size();
     }
-  }
+
+    public int size() {
+        synchronized (references) {
+            return cleanUp();
+        }
+    }
 }
