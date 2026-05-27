9601cd1c450d9c8872e66ff1a4823396768520e0
Henrib <hbiestro@gmail.com>
2026-05-27 16:36:06 +0200

    JEXL-459 : refactor exception handling and enhance test coverage for substring operations;

--- src/main/java/org/apache/commons/jexl3/JexlException.java
+++ src/main/java/org/apache/commons/jexl3/JexlException.java
@@ -23,8 +23,6 @@ import java.io.StringReader;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.UndeclaredThrowableException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 
